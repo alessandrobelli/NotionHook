a6ca7c207a0789cfbc1695837943a8717a17f103
Alessandro Belli <alessandrobelli90@gmail.com>
2022-02-12 17:48:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function createCommit(notion, commits, files) {
     array.forEach((element) => {
       description += " " + element;
     });
-    //
+
     description += "\n" + files;
 
     notion.pages.create({
