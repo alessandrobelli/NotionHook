9779f948a1c0946c2e890e27cdbd38aab14dab33
Alessandro Belli <alessandrobelli90@gmail.com>
2022-02-12 21:53:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ async function getFiles() {
         break;
     }
 
-    // Log the output values.
+    // Log the output values.s
     // core.info(`All: ${allFormatted}`);
     // core.info(`Added: ${addedFormatted}`);
     // core.info(`Modified: ${modifiedFormatted}`);
